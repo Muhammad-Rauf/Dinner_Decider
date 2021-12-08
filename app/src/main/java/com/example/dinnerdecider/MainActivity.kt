11420a633e2b4a4a4d2bfb7d114f1147396981af
Muhammad rauf <muhammadrauf048@outlook.com>
2021-12-08 17:21:02 +0500

    Dinner complte

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -2,14 +2,37 @@ package com.example.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    val foodlist= arrayListOf("Chinese Rice ","Biryani","Burger"," Pizza")
+    lateinit var runtimefood: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
        var selectedfoood:TextView=findViewById(R.id.selectedfoodTextid)
-        selectedfoood.text="Burger King"
+        var newfood:EditText=findViewById(R.id.editTextfoodid)
+       // selectedfoood.text="Burger King"
+        var deciderbtn:Button=findViewById(R.id.deciderbtnid)
+
+
+        deciderbtn.setOnClickListener{
+            val random= java.util.Random()
+            val randomFood=random.nextInt(foodlist.count())
+            selectedfoood.text=foodlist[randomFood]
+
+        }
+        var addFoodBtn:Button=findViewById(R.id.addfoodbtnid)
+        addFoodBtn.setOnClickListener{
+           // runtimefood= newfood.getText().toString()
+            runtimefood= newfood.text.toString()
+            foodlist.add(runtimefood)
+            newfood.text.clear()
+        }
 
     }
 }
